3bd48f43c08259398c078a46ee55497befacc5be
Julian94 <langlo94@gmail.com>
2015-03-03 01:32:54 +0100

    Starting to improve the available amount of chars.

--- src/com/company/OneTimeBytes.java
+++ src/com/company/OneTimeBytes.java
@@ -3,75 +3,34 @@ package com.company;
 import java.io.*;
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Random;
 
 /**
- * Created by Julian on 23.02.2015.
+ * Created by Julian on 03.03.2015.
  */
-public class OTPGen {
-    static char[] c = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X',
-            'Y','Z','0','1','2','3','4','5','6','7','8','9',' ','a','b','c','d','e','f','g','h','i','j','k','l','m','m',
-            'n','o','p','q','r','s','t','u','v','w','x','y','z',',','.',';','(',')','[',']','{','}','&','%','¤','#','"',
-            '\'','!','/','\\','?','<','>','-','_','|','*'};
-
-    public static char[] genOTP(int length){
-        char[] result = new char[length];
-        Random r = new SecureRandom();
-        for (int i = 0; i < length; i++){
-            result[i] = c[r.nextInt(c.length)];
-        }
-        return result;
+public class OneTimeBytes {
+    public static byte[] getBytes(int nr) {
+        SecureRandom sr = new SecureRandom();
+        byte[] bytes = new byte[nr];
+        sr.nextBytes(bytes);
+        return bytes;
     }
 
-    public static String charToString(char[] c) {
-        String s = "";
-        for (char a: c) {
-            s += a;
+    public static char[] getChars(int nr) {
+        char[] c = new char[nr];
+        byte[] b = getBytes(nr);
+        for (int i = 0; i < nr; i++) {
+            c[i] = (char) b[i];
         }
-        return s;
+        return c;
     }
-
-    public static String getPages(int pages, int lines, int linelength) {
-        String out = "";
-        for (int p = 0; p < pages; p++) {
-            out += ("\n" + "Page " + p + "\n");
-            for (int line = 0; line < lines; line++) {
-                out += (charToString(genOTP(linelength)) + "\n");
-            }
-        }
-        return out;
-    }
-
-    public static String getCleanLines(int lines, int linelength) {
-        String out = "";
-        for (int line = 0; line < lines; line++) {
-            out += (charToString(genOTP(linelength)) + "\n");
-        }
-        return out;
-    }
-
-    public static boolean makeFile(String filename, int pages, int lines, int linelength) {
+    public static boolean writeFile(String filename, int lines, int linelength) {
         try {
             PrintWriter pw = new PrintWriter(filename);
-            pw.write(getPages(pages, lines, linelength));
+            for (int i = 0; i < lines; i++) {
+                pw.write(getChars(linelength));
+            }
             pw.close();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        return true;
-    }
-
-    public static boolean makeCleanFile(String filename, int lines, int linelength) {
-        try {
-            FileWriter fw = new FileWriter(filename);
-            BufferedWriter bw = new BufferedWriter(fw);
-            //Writer pw = new Writer(filename);
-            bw.write(getCleanLines(lines, linelength));
-            bw.close();
-            fw.close();
-        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
@@ -100,7 +59,7 @@ public class OTPGen {
             output[i] = messages.get(i);
         }
         return output;
-    }
+    }/*
 
     public static String encrypt(String text, String cipher) {
         text = text.toUpperCase();
@@ -138,5 +97,5 @@ public class OTPGen {
             text[i] = l <= k ? c[k-l] : c[c.length - (l-k)];
         }
         return new String(text);
-    }
+    }*/
 }
